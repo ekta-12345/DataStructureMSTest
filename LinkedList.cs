113a6e927a0769c08b5c607ce578c962df1fcd0b
ekta12345 <ektakumbhare3@gmail.com>
2021-05-03 23:03:05 +0530

    First Commit

---
+++ LinkedList.cs
@@ -0,0 +1,144 @@
+﻿using System;
+
+namespace DataStructure
+{
+    class LinkedList
+    {
+        public Node head;
+        public void InsertLast(int new_data)
+        {
+            Node new_node = new Node(new_data);
+            if (this.head == null)
+            {
+                this.head = new_node;
+            }
+            else
+            {
+                Node lastNode = GetLastNode();
+                lastNode.next = new_node;
+            }
+            Console.WriteLine("inserted into list" + new_node.data);
+
+        }
+        public Node GetLastNode()
+        {
+            Node temp = this.head;
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+            return temp;
+        }
+        public void InsertFront(int new_data)
+        {
+            Node new_node = new Node(new_data);
+            new_node.next = this.head;
+            this.head = new_node;
+            Console.WriteLine("inserted into front" + new_node);
+        }
+
+
+
+        //internal Node InsertAtParticularPosition(int position, int data)
+        //{
+        //    if (position < 1)
+        //        Console.WriteLine("Invalid position");
+        //    if (position == 1)
+        //    {
+        //        var newNode = new Node(data);
+        //        newNode.next = this.head;
+        //        head = newNode;
+        //    }
+        //    else
+        //    {
+        //        while (position-- != 0)
+        //        {
+        //            if (position == 1)
+        //            {
+        //                Node node = new Node(data);
+        //                node.next = this.head.next;
+        //                head.next = node;
+        //                break;
+        //            }
+        //            head = head.next;
+        //        }
+        //        if (position != 1)
+        //            Console.WriteLine("Position out of range");
+        //    }
+        //    return head;
+        //}
+
+
+        internal Node DeleteFirstNode()
+        {
+            if (this.head == null)
+            {
+                return null;
+            }
+            this.head = this.head.next;
+            return this.head;
+        }
+
+        public Node DeleteLastNode()
+        {
+            Node newNode = this.head;
+            if (this.head == null)
+            {
+                return null;
+            }
+            if (this.head.next == null)
+            {
+                this.head = null;
+                return null;
+            }
+            while (newNode.next.next != null)
+            {
+                newNode = newNode.next;
+            }
+            newNode.next = null;
+            return newNode;
+        }
+        public bool Search(int input)
+        {
+            bool flag = false;
+            if (head == null)
+                Console.WriteLine("List is Empty");
+            else
+            {
+                Node temp = head;
+                while (temp != null)
+                {
+                    if (temp.data == input)
+                    {
+                        Console.WriteLine("The element is present");
+                        flag = true;
+                        break;
+                    }
+                    temp = temp.next;
+                }
+                if (!flag)
+                    Console.WriteLine("The Element is Absent");
+            }
+            return flag;
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            else
+            {
+                while (temp.next != null)
+                {
+                    Console.Write(temp.data + "->");
+                    temp = temp.next;
+                }
+                Console.WriteLine(temp.data);
+            }
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to data structure");
+            LinkedList list = new LinkedList();
+
+            list.InsertFront(70);
+            list.InsertFront(30);
+            list.InsertFront(56);
+            //list.InsertFront(70);
+            //list.InsertFront(56);
+
+            //list.InsertAtParticularPosition(2, 30);
+            //list.Display();
+            //Console.ReadLine();
+
+            //list.DeleteFirstNode();
+
+
+            //list.DeleteLastNode();
+            //list.Display();
+            list.Search(30);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
+
